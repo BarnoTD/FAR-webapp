d6fb7a26166b10b4ce4919a153d9a280ed7b9432
BarnoTD <105167354+BarnoTD@users.noreply.github.com>
2024-01-13 20:44:53 +0100

    Initial commit

---
+++ FastAPI/main.py
@@ -0,0 +1,91 @@
+from fastapi import FastAPI, HTTPException, Depends
+from typing import Annotated, List
+from sqlalchemy.orm import Session
+from pydantic import BaseModel
+from database import SessionLocal, engine
+import models
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI() #initialize main app
+
+#Handle Cross-Origin Resource Sharing
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ['*'],
+    allow_headers = ['*']
+)
+
+#Defining Models for Data in/out API
+class TransactionBase(BaseModel):
+    amount: float
+    category: str
+    description: str
+    is_income: bool
+    date: str
+
+class TransactionModel(TransactionBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+#Database Connection Setup
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+models.Base.metadata.create_all(bind=engine)
+
+# Update Transaction
+@app.post("/transactions/", response_model=TransactionModel)
+async def create_transaction(transaction: TransactionBase, db: db_dependency):
+    db_transaction = models.Transaction(**transaction.dict())
+    db.add(db_transaction)
+    db.commit()
+    db.refresh(db_transaction)
+    return db_transaction
+
+# Show Transaction
+@app.get("/transactions/", response_model=List[TransactionModel])
+async def read_transactions(db: db_dependency, skip: int = 0, limit: int = 100):
+    transactions = db.query(models.Transaction).offset(skip).limit(limit).all()
+    return transactions
+
+# Update Transaction
+@app.put("/transactions/{transaction_id}", response_model=TransactionModel)
+async def update_transaction(
+        transaction_id: int,
+        transaction: TransactionBase,
+        db: db_dependency
+):
+    db_transaction = db.query(models.Transaction).filter(models.Transaction.id == transaction_id).first()
+    if db_transaction:
+        for key, value in transaction.dict().items():
+            setattr(db_transaction, key, value)
+        db.commit()
+        db.refresh(db_transaction)
+        return db_transaction
+    else:
+        raise HTTPException(status_code=404, detail="Transaction not found")
+
+# Delete Transaction
+@app.delete("/transactions/{transaction_id}", response_model=TransactionModel)
+async def delete_transaction(transaction_id: int, db: db_dependency):
+    db_transaction = db.query(models.Transaction).filter(models.Transaction.id == transaction_id).first()
+    if db_transaction:
+        db.delete(db_transaction)
+        db.commit()
+        return db_transaction
+    else:
+        raise HTTPException(status_code=404, detail="Transaction not found")
